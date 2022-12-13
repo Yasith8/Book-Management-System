0a27e5e06769c3e5106c91fea49ff0849bdb2129
Yasith Nawodya <yasithnawodya8@gmail.com>
2022-12-13 21:52:57 +0530

    Update CLIPrograme.java

--- Bookshop Management System/src/commandlineprogramming/CLIPrograme.java
+++ Bookshop Management System/src/commandlineprogramming/CLIPrograme.java
@@ -9,32 +9,38 @@ public class CLIPrograme {
 		
 		Scanner scn = new Scanner(System.in);
 		
+		//get the First Name
 		System.out.println("Enter your First Name : ");
 		String firstName = scn.nextLine();
 		System.out.println();
 		
-        System.out.println("Enter your Last Name : ");
+		//Get the Last Name
+                System.out.println("Enter your Last Name : ");
 		String lastName = scn.nextLine();
 		System.out.println();
 		
-		
+		//Get the Gender
 		System.out.println("Select Your Gender : Male--> Enter M OR Female--> Enter F");
 		char gender = scn.next().charAt(0);
 		System.out.println();
 		
+		//Get the Age
 		System.out.println("Enter your Age : ");
 		int age = scn.nextInt();
 		System.out.println();
 		
+		//Get the Phone Number
 		System.out.println("Enter your Mobile Number : ");
 		Long mno = scn.nextLong();
 		System.out.println();
 		
+		//Get the Weight
 		System.out.println("Enter your Weight : ");
 		double weight = scn.nextDouble();
 		System.out.println();
 		
-	    System.out.println("Check Your Registration Details");
+		//Output All Data
+	        System.out.println("Check Your Registration Details");
 		System.out.println("Welcome "+firstName+" "+lastName);
 		System.out.println("Your Gender is "+gender);
 		System.out.println("Your Age is "+age);
